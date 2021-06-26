66ff7be868c91a24d6e805266a7abed5b9a3b492
Mohammed Rishad <rishad@reckonsys.com>
2021-06-26 20:25:35 +0530

    Login API

--- two_factor_auth/urls.py
+++ two_factor_auth/urls.py
@@ -5,5 +5,6 @@ import two_factor_auth.views as view
 
 urlpatterns = [
     path(r'questions', view.QuestionViewSet.as_view()),
-    path(r'login', view.ExampleView.as_view()),
+    path(r'login', view.UserLogin.as_view()),
+    path(r'2fa-verify-answer', view.VerifyAnswerViewSet.as_view())
 ]
